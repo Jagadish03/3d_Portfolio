17c1ad52418e00b785e25d1764905e198fb89165
unknown <jagadishmunavalli35@gmail.com>
2023-12-25 19:37:23 +0530

    commited on 7:37

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Rreact from 'react'
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import { Home, About, Projects, Contact } from './pages';
 
 function App() {
   return (
@@ -8,10 +9,10 @@ function App() {
       <BrowserRouter>
         <Navbar/> 
         <Routes>
-          <Route path="/" element={'Home'} />
-          <Route path="/about" element={'About'} />
-          <Route path="/projects" element={'Projects'} />
-          <Route path="/contact" element={'Contact'} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/projects" element={<Projects/>} />
+          <Route path="/contact" element={<Contact/>} />
         </Routes>
       </BrowserRouter>
     </main>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+{/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+    POPUP
+</div> */}
+function Home() {
+  return (
+    <section className='w-full h-screen relative'>
+    </section>
+  )
+}
+
+export default Home
