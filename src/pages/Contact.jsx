0cda331125fcb61b7d9f76eed316b8863f13613e
unknown <jagadishmunavalli35@gmail.com>
2023-12-30 22:07:00 +0530

    commited on 30-12-2023 10:06

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Contact() {
+  const [form, setForm] = useState({name:'', email:'', message:''})
+
+  const handleChange = () => {};
+  //^ whenever we click on input and start typing the 3d fox has to walk so it mush focus
+  const handleFoucs =() => {};
+  const handleBlur = () => {};
+
   return (
-    <div>Contact</div>
+    <section className='relative flex lg:flex-row flex-col max-container'>
+      <div className='flex-1 min-w-[50%] flex flex-col'>
+        <h1 className='head-text marker:'>Get in Touch</h1>
+
+        <form className='w-full flex flex-col gap-7 mt-14'>
+          <label className='text-black-500 font-semibold'>Name
+          <input 
+          type="text"
+          name='name'
+          className='input'
+          placeholder='Your Name'
+          required
+          value={form.name}     //& define a value by creating a new useState
+          onChange={handleChange}
+          onFocus={handleFoucs}
+          onBlur={handleBlur}
+          /> 
+          </label>
+          <label className='text-black-500 font-semibold'>Email
+          <input 
+          type="email"
+          name='email'
+          className='input'
+          placeholder='Your Email'
+          required
+          value={form.email}     //& define a value by creating a new useState
+          onChange={handleChange}
+          onFocus={handleFoucs}
+          onBlur={handleBlur}
+          /> 
+          </label>
+          <label className='text-black-500 font-semibold'>Your Message
+          <textarea
+          name='message'
+          rows={4}
+          className='textarea'
+          placeholder='Let me Know how i help you!'
+          required
+          value={form.message}     //& define a value by creating a new useState
+          onChange={handleChange}
+          onFocus={handleFoucs}
+          onBlur={handleBlur}
+          /> 
+          </label>
+        </form>
+      </div>
+    </section>
   )
 }
 
