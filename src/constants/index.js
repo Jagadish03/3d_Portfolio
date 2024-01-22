90cc3867c3b6dafc9fbeab7283e3f4cde7e3d354
jagadish03 <jagadishmunavalli35@.com>
2024-01-22 09:27:37 +0530

    Footer commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -171,12 +171,12 @@ export const socialLinks = [
     {
         name: 'GitHub',
         iconUrl: github,
-        link: 'https://github.com/YourGitHubUsername',
+        link: 'https://github.com/Jagadish03',
     },
     {
         name: 'LinkedIn',
         iconUrl: linkedin,
-        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+        link: 'https://www.linkedin.com/in/jagadish-s-munavalli-278aba219/',
     }
 ];
 
